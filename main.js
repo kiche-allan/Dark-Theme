6b059abbc9fc30d809d082628a583442e74e11d4
Allan Kiche <allankiche1@gmail.com>
2020-12-15 14:16:38 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () => {
+    //change the theme of the website
+    document.body.classList.toggle('dark');
+    
+});
